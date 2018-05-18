a1bbec9dad231e750daf73c8b75afd833d19847e
JaiminWu <jaiminwu@163.com>
2018-05-18 23:11:08 +0800

    fix

--- app/Http/Controllers/ContainerController.php
+++ app/Http/Controllers/ContainerController.php
@@ -23,7 +23,7 @@ class ContainerController extends Controller
         $client = new \GuzzleHttp\Client();
         $i = 0;
         foreach ($hosts as $key => $value) {
-          $containers = json_decode(json_encode($client->request('GET', 'http://'.$value['host'].':'.$value['port'].'/containers/json?all=1')), true);
+          $containers = $client->request('GET', 'http://'.$value['host'].':'.$value['port'].'/containers/json?all=1');
           // Container::where(['host_id' => $value['id'],])->get();
           // foreach ($containers as $v) {
             $host[$i] = $containers;
